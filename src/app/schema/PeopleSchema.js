abfa80f8428840b79585989bb942bcaba8c09abb
adson-SO <adson.sousa.27@gmail.com>
2022-01-27 12:20:26 -0300

    chore: add people schema

---
+++ src/app/schema/PeopleSchema.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const mongoosePaginate = require('mongoose-paginate-v2');
+
+const PeopleSchema = mongoose.Schema({
+    nome: String,
+    cpf: {
+        type: String,
+        unique: true
+    },
+    data_nascimento: Date,
+    email: String,
+    senha: String,
+    habilitado: {
+        type: String,
+        enum: ['sim', 'não']
+    }
+});
+
+PeopleSchema.plugin(mongoosePaginate);
+
+const People = mongoose.model('People', PeopleSchema);
+
+module.exports = People;
