9ac7749dd13100e38b55a4cb514154fff6ff01c3
adson-SO <adson.sousa.27@gmail.com>
2022-02-01 16:10:34 -0300

    fix: add age validation for people, allowing only people that are 18 or older to be registered

--- src/app/validations/people/create.js
+++ src/app/validations/people/create.js
@@ -1,11 +1,13 @@
 const Joi = require('joi').extend(require('@joi/date'));
 
+const birthDate = new Date((Date.now()) - (1000 * 60 * 60 * 24 * 365 * 18));
+
 module.exports = async (req, res, next) => {
     try {
         const schema = Joi.object({
             nome: Joi.string().min(3).required(),
             cpf: Joi.string().length(11).pattern(/^[0-9]+$/).required(),
-            data_nascimento: Joi.date().format('DD/MM/YYYY').raw().required(),
+            data_nascimento: Joi.date().format('DD/MM/YYYY').raw().max(birthDate).required(),
             email: Joi.string().email().required(),
             senha: Joi.string().min(6).required(),
             habilitado: Joi.string().valid('sim', 'não').required()
--- src/app/validations/people/update.js
+++ src/app/validations/people/update.js
@@ -1,11 +1,13 @@
 const Joi = require('joi').extend(require('@joi/date'));
 
+const birthDate = new Date((Date.now()) - (1000 * 60 * 60 * 24 * 365 * 18));
+
 module.exports = async (req, res, next) => {
     try {
         const schema = Joi.object({
             nome: Joi.string().min(3),
             cpf: Joi.string().length(11).pattern(/^[0-9]+$/),
-            data_nascimento: Joi.date().format('DD/MM/YYYY').raw(),
+            data_nascimento: Joi.date().format('DD/MM/YYYY').raw().max(birthDate),
             email: Joi.string().email(),
             senha: Joi.string().min(6),
             habilitado: Joi.string().valid('sim', 'não')
