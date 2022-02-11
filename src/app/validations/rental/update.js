67e0a1f4256654e0bad8653e6376e7212a9904af
adson-SO <adson.sousa.27@gmail.com>
2022-02-11 12:25:02 -0300

    chore: add validation on PUT route of rentals

--- src/app/validations/rental/update.js
+++ src/app/validations/rental/update.js
@@ -4,25 +4,25 @@ const cnpjValidate = require('../../../helpers/cnpjValidate');
 module.exports = async (req, res, next) => {
     try {
         const schema = Joi.object({
-            nome: Joi.string().min(5).required(),
+            nome: Joi.string().min(5),
             cnpj: Joi.string().length(18).custom((value, helpers) => {
-                if(cnpjValidate(value) === false) {
+                if (cnpjValidate(value) === false) {
                     return helpers.message('CNPJ is invalid');
                 } else {
                     return true;
                 }
-            }).required(),
-            atividades: Joi.string().min(10).required(),
+            }),
+            atividades: Joi.string().min(10),
             endereco: Joi.array().min(1).items(Joi.object({
-                cep: Joi.string().length(9).required(),
+                cep: Joi.string().length(9),
                 complemento: Joi.string().min(3),
-                number: Joi.string().min(1).required(),
-                isFilial: Joi.boolean().required()
+                number: Joi.string().min(1),
+                isFilial: Joi.boolean()
             }))
         });
 
         const { error } = await schema.validate(req.body, { abortEarly: true });
-        if(error) throw error;
+        if (error) throw error;
         return next();
     } catch (err) {
         return res.status(400).json({
