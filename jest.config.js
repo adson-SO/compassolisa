b3d2de3eb01ff7339bdc08f7f14a8e9c426c96fe
adson-SO <adson.sousa.27@gmail.com>
2022-02-15 16:41:12 -0300

    chore: add initial test configurations

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    bail: true,
+    clearMocks: true,
+    coverageProvider: "v8",
+    testEnvironment: "node",
+    testMatch: [
+        "**/__tests__/**/*.test.js?(x)"
+    ]
+};
