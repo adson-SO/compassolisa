6abdce893c6f97af574987270866c60230778f47
adson-SO <adson.sousa.27@gmail.com>
2022-02-15 17:26:55 -0300

    refactor: change variable 'port' to environment variable from 'dotenv' config

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 const app = require('./app');
-const port = 3000;
+require('dotenv').config();
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Server is running!');
 });
