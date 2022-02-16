e43da5134a87d2bec744017dd26c51639c94dfb8
adson-SO <adson.sousa.27@gmail.com>
2022-02-16 17:03:25 -0300

    fix: update validation on route POST of rentals

--- src/app/validations/rental/create.js
+++ src/app/validations/rental/create.js
@@ -4,7 +4,7 @@ const cnpjValidate = require('../../../helpers/cnpjValidate');
 module.exports = async (req, res, next) => {
   try {
     const schema = Joi.object({
-      nome: Joi.string().min(5).required(),
+      nome: Joi.string().min(5).trim().required(),
       cnpj: Joi.string()
         .length(18)
         .custom((value, helpers) => {
@@ -14,17 +14,17 @@ module.exports = async (req, res, next) => {
           return true;
         })
         .required(),
-      atividades: Joi.string().min(10).required(),
+      atividades: Joi.string().min(10).trim().required(),
       endereco: Joi.array()
         .min(1)
         .items(
           Joi.object({
-            cep: Joi.string().length(9).required(),
-            complemento: Joi.string().min(3),
-            number: Joi.string().min(1).required(),
+            cep: Joi.string().length(9).trim().required(),
+            complemento: Joi.string().min(3).trim(),
+            number: Joi.string().min(1).trim().required(),
             isFilial: Joi.boolean().required()
           })
-        )
+        ).required()
     });
 
     const { error } = await schema.validate(req.body, { abortEarly: true });
