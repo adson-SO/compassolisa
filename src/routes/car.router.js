291f8ffbdf33bdfb3205846582d32ab168ec8469
adson-SO <adson.sousa.27@gmail.com>
2022-02-16 11:58:49 -0300

    fix: add validation to route DELETE of cars

--- src/routes/car.router.js
+++ src/routes/car.router.js
@@ -3,11 +3,12 @@ const createValidation = require('../app/validations/car/create');
 const findValidation = require('../app/validations/car/find');
 const findByIdValidation = require('../app/validations/car/findById');
 const updateValidation = require('../app/validations/car/update');
+const deleteValidation = require('../app/validations/car/delete');
 
 module.exports = (server, routes, prefix = '/api/v1/car') => {
   routes.post('/', createValidation, CarController.create);
   routes.get('/', findValidation, CarController.find);
-  routes.delete('/:id', CarController.delete);
+  routes.delete('/:id', deleteValidation, CarController.delete);
   routes.put('/:id', updateValidation, CarController.update);
   routes.get('/:id', findByIdValidation, CarController.findById);
   routes.patch('/:id/acessorios/:descricaoId', CarController.updateAcessorio);
