e9bd32d49e23a0f858b0f483e20a906cd93cea79
adson-SO <adson.sousa.27@gmail.com>
2022-02-20 18:58:49 -0300

    fix: update path to swagger file in swagger route

--- src/routes/docs/swagger.router.js
+++ src/routes/docs/swagger.router.js
@@ -1,5 +1,5 @@
 const swaggerUi = require('swagger-ui-express');
-const swaggerDoc = require('../../../compassolisa_swagger.json');
+const swaggerDoc = require('../../../swagger.json');
 
 module.exports = (server, routes, prefix = '/api/v1/api-docs') => {
   routes.use('/', swaggerUi.serve);
